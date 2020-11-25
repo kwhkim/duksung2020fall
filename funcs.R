1bc745078fccfca5dcf413e12c294c3355d7b638
kwhkim <kwonhyun.kim@gmail.com>
2020-11-25 10:54:39 +0900

    L12_path analysis

--- funcs.R
+++ funcs.R
@@ -7,4 +7,10 @@ range2 = function(x) {
   max(x) -min(x)
 }
 
+x_label_vertical = function() {
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+}
 
+legend_col_alpha1 = function() {
+  guides(colour = guide_legend(override.aes=list(alpha=1)))
+}
